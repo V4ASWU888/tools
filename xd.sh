26637cb1c0c64c8de2d806ad1ff7fb70e476f272
V4ASWU888 <77200273+V4ASWU888@users.noreply.github.com>
2021-02-06 23:57:45 +0700

    xd.sh

---
+++ xd.sh
@@ -0,0 +1,78 @@
+#!/systeam/xbin/bash
+#mau recode dek
+blue='\033[34;1m'
+green='\033[32;1m'                                        
+purple='\033[35;1m'
+cyan='\033[36;1m'
+red='\033[31;1m'
+white='\033[37;1m'                                           
+yellow='\033[33;1m'
+sleep 1
+toilet -f big -F gay "T00Ls" | lolcat
+toilet -f big -F gay "hakikixd" 
+clear
+echo ""      
+echo "\033[32;1mAuthor : Mr.H4K1K1 " 
+echo "\033[35;1mTeam : White hat"
+echo "\033[33;1mKontak Me : 085946352369"
+echo "\033[31;1mSubcribe Youtube:"
+echo "\033[33;1mwhite hat" "\033[32;1m&" "\033[36;1mIndonesia Security Lite"
+echo "\033[34;1m[============================[>"
+sleep 1
+echo ""
+echo "\033[32;1mSilahkan Pilih Tools Yang mau Kamu pake:)?:"
+echo "\033[36;1m"
+echo "--------------"
+echo $red      "(1)spam sms"
+echo $yellow   "(2)crackfb"
+echo $green    "(3)install metasploit"
+echo $blue      "(0)exit"
+echo "\033[32;1m"
+read -p "[root@T00Ls-hakikixd:~#" bro
+
+if [ $bro = 1 ] || [ $bro = 1 ]
+then
+clear
+figlet "hakikixd" | lolcat
+ pkg update && pkg upgrade
+ pkg install git 
+ pkg install python
+ pip install requests
+ git clone https://github.com/KINGTEBE-404/Brutal-Sms
+ cd Brutal-Sms
+python brutal.py
+fi
+
+if [ $bro = 2 ] || [ $bro = 2]
+then
+clear
+figlet "hakikixd" | lolcat
+pkg install python2 git
+git clone https://github.com/Rizal-XD/krek
+cd krek
+python2 run.py
+fi
+
+if [ $bro = 3 ] || [ $bro = 3 ]
+then
+clear
+figlet "hakikixd" | lolcat
+pkg install python
+pkg install python2
+pkg install wget
+pkg install curl
+pkg install nmap
+pkg install ruby
+pkg install bash
+curl -LO raw.githubusercontent.com/Hax4us/Metasploit_termux/master/metasploit.sh
+chmod 777 metasploit.sh
+./metasploit.sh
+cd metasploit-framework
+bundle update --bundler
+msfconsole
+fi
+
+if [ $bro = 0  ] || [ $bro = 0 ]
+then
+exit
+fi
